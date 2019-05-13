df8bbf03abca6cc6a9318fc12cac7d9c4dad092b
Oliver Brownlow <olliebrownlow@gmail.com>
2019-05-13 16:22:49 +0100

    Sinatra: Returning HTML

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
 
 get '/' do
   "hello Michael!"
@@ -7,4 +8,16 @@ end
 
 get '/secret' do
   "Smoking kills!"
+end
+
+get '/cat' do
+  "
+  <head>
+  'OK'
+  </head>
+  <div style='border: 30px solid blue'>
+    <img src='http://bit.ly/1eze8aE'>
+   </div>
+   <tail>'GO'
+   </tail>"
 end
