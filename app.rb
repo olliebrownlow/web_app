424c9d064743f7f7feb45589b1c53ca02defd069
Oliver Brownlow <olliebrownlow@gmail.com>
2019-05-13 17:40:00 +0100

    Sinatra: Introducing params

--- app.rb
+++ app.rb
@@ -10,7 +10,14 @@ get '/secret' do
   "Smoking kills!"
 end
 
-get '/cat' do
+get '/random-cat' do
   @random_name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @random_name = params[:name]
+  @age = params[:age]
   erb(:index)
 end
